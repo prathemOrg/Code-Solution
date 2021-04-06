9ba3aa889888f7f7d12e586b235732e750c2c9fd
prathem-cennest <prathemesh.sangle@cennest.com>
2021-04-06 23:55:00 +0530

    filestructure

---
+++ CodeSolution/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSolution
+{
+    class Class1
+    {
+       static void Main()
+        {
+            DataManager dm = new DataManager();
+            /* User u = new User();
+             u.UserFirstName = "vijay";
+             u.UserLastName = "amni";
+             u.UserEmail = "va.va@gmail.com";
+             u.UserPassword = "1234";
+             dm.AddUser(u);
+             */
+            foreach (User user in dm.GetAllUsers())
+            {
+                Console.WriteLine(user.UserFirstName);
+               
+            }
+            dm.DeleteUser(1);
+            Console.ReadLine();
+        }
+        
+    }
+}
